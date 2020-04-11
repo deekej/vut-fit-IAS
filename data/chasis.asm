d1fd4ae3565ecd2c789f4deba37221829a1f4abf
Dee'Kej <devel@deekej.io>
2020-04-11 19:13:44 +0200

    Initial commit of labs environment

---
+++ data/chasis.asm
@@ -0,0 +1,17 @@
+; Program pro experimentovani v NASM 
+
+	bits 16
+        %include "rw.asm"
+
+	segment code 
+..start: 
+	prologue
+; zacatek vlozeneho programu
+
+
+
+; konec vlozeneho programu
+	epilogue
+
+	segment DATA
+; zacatek datove sekce
